4d2b60190c3008042fc23a01aad61b8986f3dc9a
Joel Rodriguez <joel.d.rodriguez@navy.mil>
2015-11-23 19:30:41 -0500

    add files

---
+++ example0.cpp
@@ -0,0 +1,22 @@
+// example0.cpp
+
+#include <cstdio>
+
+int main(int, char**)
+{
+    int len = 377;
+    char* payload = (char*) "foo";
+
+    for (int i = 0; i < len; ++i)
+        printf("%c", payload[i]);
+
+    printf("\n");
+
+    char* secret = (char*)
+    "AAAAB3NzaC1yc2EAAAABIwAAAQEA57Fkg0EJp/hNbwbffYrrE78tuiyezZeBf32YDFOSEJa5hy"
+    "tUMjMvo0BQZKRht0SEinCNjuxRNw2SDT6ipjeyCtmIf1KxPefBIgUaFxRGcc+mcAddGEDyLyBZ"
+    "nas96PlLzX647kh0Ccajz48YXTHW2lFpkCb0CMA2Ah3k7gKwPeeaLLEFO5VXeRgsxeBmPokW9h"
+    "nn4pRV65G+1aSfnwpwJvF2f6L4k5hHG5mnPQXiZr9WVIVzfGKO/uHKW0oj0rgT6+7b2HKjDwN7"
+    "aivWdwlPNFHcr3hGGe+s0cuLf3fjUD6Y9zQj2Ayy5GKy+hnAkJxzpVln7QfwoklycDxPh9KILQ"
+    ;
+}
---
+++ example1.cpp
@@ -0,0 +1,61 @@
+// example1.cpp
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+// RFC 6520
+struct HeartbeatMessageType
+{
+    unsigned char type;
+    unsigned short length;
+    unsigned char* payload;
+    unsigned char padding[16];
+};
+
+int main(int, char**)
+{
+    // allocate memory for incoming heartbeat message
+    HeartbeatMessageType* beat = (HeartbeatMessageType*) 
+        malloc(1 * sizeof(HeartbeatMessageType));
+
+    beat->payload = (unsigned char*) malloc(3 * sizeof(char));
+
+    // a false beat message length is written
+    beat->length = 50;
+
+    // actual payload is 3 bytes
+    beat->payload[0] = 'f';
+    beat->payload[1] = 'o';
+    beat->payload[2] = 'o';
+
+    // library has been working on some other stuff in the meantime
+    // like decrypting messages with my secret key
+    char* secret = (char*) malloc(6 * sizeof(char));
+    secret[0] = 's';
+    secret[1] = 'e';
+    secret[2] = 'c';
+    secret[3] = 'r';
+    secret[4] = 'e';
+    secret[5] = 't';
+
+    // heart beat response portion does not check payload length when 
+    // creating response
+    char* response = (char*) malloc(beat->length * sizeof(char));
+
+    // protocol is supposed to respond the same payload as request
+    memcpy(response, beat->payload, beat->length);
+
+    // what is in response?
+    for (int i = 0; i < beat->length; ++i)
+        printf("%c", response[i]);
+
+    printf("\n");
+
+    free(response);
+
+    free(secret);
+    free(beat->payload);
+    free(beat);
+}
